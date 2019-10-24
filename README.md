b628d795c4aedbfe1755044b8dfc541f06799d89
Sean McGrath <sean@mcgrath.net.nz>
2019-10-25 11:54:50 +1300

    Updates

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # action-gcr-push
 Build, tag, and push container to Google Cloud Registry
+
+## Example
+
+```yml
+name: Build and push to GCR
+on:
+  push:
+    branches:
+      - master
+jobs:
+  build_push_gcr:
+    runs-on: ubunti-latest
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v1
+
+      - name: Build and push to GCR
+        uses: wipash/action-gcr-push@master
+        env:
+          GCLOUD_AUTH_KEY: ${{secrets.GCLOUD_AUTH_KEY}}
+        with:
+          image: cmsback
+          version_tag: 1.0.2
+          environment: staging
+
+```
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,31 @@
 set -e
 
 REGISTRY=${INPUT_REGISTRY:-gcr.io}
-IMAGE=${INPUT_IMAGE:-$GITHUB_REPOSITORY}
-TAG=${INPUT_TAG:-$GITHUB_SHA}
+IMAGE=${INPUT_IMAGE}
+ENVIRONMENT=${INPUT_ENVIRONMENT:-development}
+TAG=${INPUT_VERSION_TAG}
 
 if [ -n "${GCLOUD_AUTH_KEY}" ]
 then
     echo "${GCLOUD_AUTH_KEY}" | base64 --decode > /tmp/gcloud_key.json
     gcloud auth activate-service-account --quiet --key-file /tmp/gcloud_key.json
     gcloud auth configure-docker --quiet
+    PROJECT=$(gcloud config get-value project 2>/dev/null)
 else
     echo "GCLOUD_AUTH_KEY not provided, exiting" 1>&2
     exit 1
 fi
 
-sh -c "docker build -t $IMAGE:$TAG ."
-sh -c "docker tag $IMAGE:$TAG $REGISTRY/$IMAGE:$TAG"
-sh -c "docker push $REGISTRY/$IMAGE"
+sh -c "docker build -t $IMAGE:$GITHUB_SHA"
+
+sh -c "docker tag $IMAGE:$GITHUB_SHA $REGISTRY/$PROJECT/$ENVIRONMENT/$IMAGE:$GITHUB_SHA"
+sh -c "docker tag $IMAGE:$GITHUB_SHA $REGISTRY/$PROJECT/$ENVIRONMENT/$IMAGE:latest"
+
+if [ -n "${TAG}" ]
+then
+    sh -c "docker tag $IMAGE:$GITHUB_SHA $REGISTRY/$PROJECT/$ENVIRONMENT/$IMAGE:${TAG}"
+    sh -c "docker push $REGISTRY/$PROJECT/$ENVIRONMENT/$IMAGE:${TAG}"
+fi
+
+sh -c "docker push $REGISTRY/$PROJECT/$ENVIRONMENT/$IMAGE:$GITHUB_SHA"
+sh -c "docker push $REGISTRY/$PROJECT/$ENVIRONMENT/$IMAGE:latest"
